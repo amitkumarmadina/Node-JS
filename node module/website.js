9341fff95b77e1de4afab52357d6f6910d0e6664
AMIT KUMAR MADINA <amitkumarmadina9@gmail.com>
2025-12-20 10:58:40 +0530

    commit

--- node module/website.js
+++ node module/website.js
@@ -1,24 +1,36 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+  console.log(req.url);
 
-    if(req.url === '/'){
-        res.statusCode = 200;
-        res.end('Welcome to our homepage');
-    }else if(req.url === '/about'){
-        res.statusCode = 200;
-    res.end('this is about page');
-    }else{
-        res.statusCode = 404;
-        res.end('404 page not found');
-    }
+  if (req.url === "/") {
+    const filePath = path.join(__dirname, "index.html");
+    const data = fs.readFileSync(filePath);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+
+  } else if (req.url == "/about") {
+    const filePath = path.join(__dirname, "index.html");
+    const data = fs.readFileSync(filePath);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+
+  } else if (req.url == "/link") {
+    const filePath = path.join(__dirname, "Link.html");
+    const data = fs.readFileSync(filePath);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>404 Not Found</h1>");
+  }
 });
 
 server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
